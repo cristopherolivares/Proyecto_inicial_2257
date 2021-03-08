347f4e58e05af6db186839398419a72d4d17f7a7
cristopherolivares <cristopherolivares90@outlook.com>
2021-03-08 17:21:02 -0600

    trabajo 08/03/21

---
+++ src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.aragon.fes.persistencia;
+
+import java.util.ArrayList;
+import mx.unam.aragon.fes.Empleado;
+
+/**
+ *
+ * @author cristopher
+ */
+public class ArchivoEmpleado {
+    private String archivo;
+
+    public ArchivoEmpleado() {
+    }
+
+    public ArchivoEmpleado(String archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    public void guardarEmpleados(ArrayList<Empleado> datos ){
+        //aqué el código para guardar en disco duro
+    }
+    public ArrayList<Empleado> leerEmpleados(){
+        ArrayList<Empleado> tmp=null;
+        //leer del disco duro 
+        //se sube al arraylist
+        return tmp;
+    }
+}
